580c77b35a8d7256769bc1d3f31c95166ee5fabb
Airton Ponce <ponceairton@gmail.com>
2020-08-24 18:46:22 -0300

    switch responses according to request verb

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
-import(
+import (
 	"log"
 	"net/http"
 )
@@ -9,11 +9,27 @@ type server struct{}
 
 func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"message":"hello world"}`))
+
+	switch r.Method {
+	case "GET":
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"message":"GET called"}`))
+	case "POST":
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte(`{"message":"POST called"}`))
+	case "PUT":
+		w.WriteHeader(http.StatusAccepted)
+		w.Write([]byte(`{"message":"PUT called"}`))
+	case "DELETE":
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"message":"DELETE called"}`))
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte(`{"message":"not found"}`))
+	}
 }
 func main() {
 	s := &server{}
 	http.Handle("/", s)
-	log.Fatal(http.ListenAndServe(":8080",nil))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
